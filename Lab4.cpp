adc3fc52fa0c8669ef65d210d26a445bbd1d128d
cakesonia <32911972+cakesonia@users.noreply.github.com>
2017-12-10 16:32:24 +0200

    Update Lab4.cpp

--- Lab4.cpp
+++ Lab4.cpp
@@ -10,7 +10,7 @@ class Matrix
 private:
 	int a[n][n];
 	int column_sum[n];
-	int i, j, k, c, product;
+	int i, j, k, c, product; // i - row, j - column
 public:
 	void matrix_input();
 	void matrix_output();
